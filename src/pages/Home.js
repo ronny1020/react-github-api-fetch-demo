a0ae9adabe7e6691f8cad5f249904746c5552f2e
Ronny Hsieh <1020ronny@gmail.com>
2020-05-27 18:24:28 +0800

    show the data on page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,13 +5,30 @@ import { getProduct } from '../actions/getGitHubAPI'
 function Home(props) {
   const { getProduct, repos } = props
 
+  const columns = ['id', 'name', 'description', 'size', 'url']
+
   useEffect(() => {
     getProduct('ronny1020')
   }, [getProduct])
 
+  const thead_th = columns.map((col) => <th scope="col">{col}</th>)
+  const tbody_tr = repos.map((row) => (
+    <tr>
+      {columns.map((col) => (
+        <th scope="col">{row[col]}</th>
+      ))}
+    </tr>
+  ))
+
   return (
     <div className="container">
-      <h1>Index</h1>
+      <h1 className="m-5">GitHubAPI DEMO</h1>
+      <table className="table table-striped">
+        <thead className="thead-dark">
+          <tr>{thead_th}</tr>
+        </thead>
+        <tbody>{tbody_tr}</tbody>
+      </table>
     </div>
   )
 }
