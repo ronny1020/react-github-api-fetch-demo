dd36e4c51f24bf385587fdea211a01db81fd5a5b
Ronny Hsieh <1020ronny@gmail.com>
2020-05-27 22:16:13 +0800

    Infinite Scroll

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,25 +4,62 @@ import { getProduct } from '../actions/getGitHubAPI'
 
 function Home(props) {
   const { getProduct, repos } = props
+  const [user, setUser] = React.useState('ronny1020')
+  const [numOfRows, setNumOfRows] = React.useState(1)
 
   const columns = ['id', 'name', 'description', 'size', 'url']
 
   useEffect(() => {
-    getProduct('ronny1020')
-  }, [getProduct])
+    getProduct(user)
+  }, [getProduct, user])
 
-  const thead_th = columns.map((col) => <th scope="col">{col}</th>)
-  const tbody_tr = repos.map((row) => (
-    <tr>
-      {columns.map((col) => (
-        <th scope="col">{row[col]}</th>
-      ))}
-    </tr>
+  useEffect(() => {
+    if (
+      document.documentElement.scrollHeight === window.innerHeight &&
+      window.scrollY === 0 &&
+      numOfRows < repos.length
+    ) {
+      setNumOfRows(numOfRows + 1)
+    }
+    const handleScroll = () => {
+      if (
+        window.scrollY + window.innerHeight ===
+          document.documentElement.scrollHeight &&
+        numOfRows < repos.length
+      ) {
+        setNumOfRows(numOfRows + 1)
+      }
+    }
+    window.addEventListener('scroll', handleScroll)
+    return () => {
+      window.removeEventListener('scroll', handleScroll)
+    }
+  }, [numOfRows, repos.length])
+
+  const thead_th = columns.map((col, i) => (
+    <th key={i} scope="col">
+      {col}
+    </th>
   ))
+  const tbody_tr = repos.map((row, i) => {
+    if (i < numOfRows) {
+      return (
+        <tr key={i}>
+          {columns.map((col, i) => (
+            <th key={i} scope="col">
+              {row[col]}
+            </th>
+          ))}
+        </tr>
+      )
+    }
+    return null
+  })
 
   return (
     <div className="container">
       <h1 className="m-5">GitHubAPI DEMO</h1>
+
       <table className="table table-striped">
         <thead className="thead-dark">
           <tr>{thead_th}</tr>
