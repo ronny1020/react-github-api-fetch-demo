405c3c8b8fd25accc9324b809ca70db3c4d004e9
Ronny Hsieh <1020ronny@gmail.com>
2020-05-28 11:10:58 +0800

    input

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,7 +4,8 @@ import { getProduct } from '../actions/getGitHubAPI'
 
 function Home(props) {
   const { getProduct, repos } = props
-  const user = 'ronny1020'
+
+  const [user, setUser] = React.useState('ronny1020')
   const [numOfRows, setNumOfRows] = React.useState(1)
 
   const columns = ['id', 'name', 'description', 'size', 'url']
@@ -36,35 +37,70 @@ function Home(props) {
     }
   }, [numOfRows, repos.length])
 
-  const thead_th = columns.map((col, i) => (
-    <th key={i} scope="col">
-      {col}
-    </th>
-  ))
-  const tbody_tr = repos.map((row, i) => {
-    if (i < numOfRows) {
-      return (
-        <tr key={i}>
-          {columns.map((col, i) => (
-            <th key={i} scope="col">
-              {row[col]}
-            </th>
-          ))}
-        </tr>
-      )
-    }
-    return null
-  })
+  let result = (
+    <div className="alert alert-warning">
+      <strong>Warning!</strong> <span>Can't find data</span>
+    </div>
+  )
+  if (repos.length) {
+    const thead_th = columns.map((col, i) => (
+      <th key={i} scope="col">
+        {col}
+      </th>
+    ))
+    const tbody_tr = repos.map((row, i) => {
+      if (i < numOfRows) {
+        return (
+          <tr key={i}>
+            {columns.map((col, i) => (
+              <th key={i} scope="col">
+                {row[col]}
+              </th>
+            ))}
+          </tr>
+        )
+      }
+      return null
+    })
 
-  return (
-    <div className="container">
-      <h1 className="m-5">GitHubAPI DEMO</h1>
-      <table className="table table-striped">
+    result = (
+      <table className="table table-striped my-5">
         <thead className="thead-dark">
           <tr>{thead_th}</tr>
         </thead>
         <tbody>{tbody_tr}</tbody>
       </table>
+    )
+  }
+
+  return (
+    <div className="container">
+      <h1 className="my-5">GitHubAPI DEMO</h1>
+
+      <div className="my-5">
+        <label htmlFor="userId">Write the GitHub ID to search here:</label>
+        <div className="input-group mb-3">
+          <input
+            type="text"
+            className="form-control"
+            placeholder="ID"
+            id="userId"
+            name="userId"
+            defaultValue={user}
+          />
+          <div className="input-group-append">
+            <button
+              className="btn btn-success"
+              onClick={() => {
+                setUser(document.getElementById('userId').value)
+              }}
+            >
+              Search ID
+            </button>
+          </div>
+        </div>
+      </div>
+      {result}
     </div>
   )
 }
